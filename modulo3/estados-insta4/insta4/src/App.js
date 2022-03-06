c353ec85119e9d3dfa7086ccddc408acf38bf182
alainn-n <contato.alain@gmail.com>
2022-03-06 13:53:30 -0300

    Entrega Exercicios Aula - Introdução ao React + JSX

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -10,27 +10,99 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150"
+      },
+
+      {
+        nomeUsuario: "alain",
+        fotoUsuario: "https://picsum.photos/49/50",
+        fotoPost: "https://picsum.photos/199/150"
+      },
+
+      {
+        nomeUsuario: "christian",
+        fotoUsuario: "https://picsum.photos/51/50",
+        fotoPost: "https://picsum.photos/201/150"
+      }
+    
+    ],
+
+    valorInputUser: "",
+    valorInputFotoPerfil: "",
+    valorInputPost: ""
+    
+  };
+
+
+
+  publicar = () => {
+    const novaPubli = {
+      usuario: this.state.valorInputUser,
+      fotoPerfil: this.state.valorInputFotoPerfil,
+      fotoPost: this.state.valorInputPost,
+      };
+
+    const novoPosts = [...this.state.posts, novaPubli];
+    this.setState({ posts: novoPosts });
+  };
+
+  onChangeInputUser = (event) => {
+    this.setState({ valorInputUser: event.target.value });
+  };
+
+  onChangeInputFotoPerfil = (event) => {
+    this.setState({ valorInputFotoPerfil: event.target.value });
+  };
+
+  onChangeInputPost = (event) => {
+    this.setState({ valorInputPost: event.target.value });
+  };
+  
+
+
   render() {
+    const arrayPostagens = this.state.posts.map((postagem) => {
+      return (
+        <Post
+          nomeUsuario={postagem.nomeUsuario}
+          fotoUsuario={postagem.fotoUsuario}
+          fotoPost={postagem.fotoPost}
+        />
+      );
+    });
+
+    
+
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
+       <MainContainer>
+        <div>{arrayPostagens}</div>
 
-        <Post
-          nomeUsuario={'alain'}
-          fotoUsuario={'https://picsum.photos/49/50'}
-          fotoPost={'https://picsum.photos/199/150'}
-        />
+        <div>
+          <input
+            value={this.state.valorInputUser}
+            onChange={this.onChangeInputUser}
+            placeholder={"Usuario"}
+          />
+          <input
+            value={this.state.valorInputFotoPerfil}
+            onChange={this.onChangeInputFotoPerfil}
+            placeholder={"Foto Perfil"}
+          />
 
-        <Post
-          nomeUsuario={'christian'}
-          fotoUsuario={'https://picsum.photos/51/50'}
-          fotoPost={'https://picsum.photos/201/150'}
-        />
-      </MainContainer>
+          <input
+            value={this.state.valorInputPost}
+            onChange={this.onChangeInputPost}
+            placeholder={"Foto Post"}
+          />
+          <button onClick={this.publicar}>Adicionar</button>
+        </div>
+
+       </MainContainer>
     );
   }
 }
