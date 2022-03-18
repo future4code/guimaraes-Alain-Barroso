256169533f5783ffe1ad326ca29e4ac4ccc61973
alainn-n <contato.alain@gmail.com>
2022-03-18 13:57:26 -0300

    erro commit ao tentar sair da branch, refazendo

--- modulo3/use-effect/aula32-use-effect/src/components/PokeCard/PokeCard.js
+++ modulo3/use-effect/aula32-use-effect/src/components/PokeCard/PokeCard.js
@@ -1,60 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const PokeCard = () => {
+const PokeCard = (props)=> {
 
+    const [pokemon, setPokemon] = useState({})
 
-    const [pokemons, setPokemons] = useState([])
-
-  
-//   componentDidMount() {
-//     this.pegaPokemon(this.props.pokemon);
-//   }
-
-//   componentDidUpdate(prevProps) {
-
-//     if (prevProps.pokemon !== this.props.pokemon) {
-//       this.pegaPokemon(this.props.pokemon);
-//     }
-//   }
-
-
-//   pegaPokemon = pokeName => {
-//     axios
-//       .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-//       .then(response => {
-//         // guarda as infos do pokemon no estado
-//         this.setState({ pokemon: response.data });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//   };
-
-  useEffect(() => {
-    const fetchPokemons = async () =>{
-        const {data} = axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemons}`)
-        console.log(data)
-    }
-    
-    fetchPokemons();
-
-  }, [])
-
-    return (
-        <div>
-            {/* {user.map((pokemon, i) => {
-                {pokemon.name}
-                {pokemon.weight} Kg
-                {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
-                {pokemon.sprites && (
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        useEffect(() => {
+            const pegaPokemon = async () => {
+                try {
+                    const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${props.pokemon}`)
+                    setPokemon(data)
+                }   catch(error) {
+                    console.log(error)
                 }
-            )} */}
+            }
 
-        </div>
+            pegaPokemon()
+        }, [props.pokemon])
+  
+
+      return (
+      <div>
+        <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+      </div>
     );
-  }
 
+}
 
-// export default PokeCard;
+export default PokeCard;
