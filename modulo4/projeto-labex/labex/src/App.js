3c55d58aa1f2036197b343eff07640c71b0f18ea
alainn-n <contato.alain@gmail.com>
2022-03-31 23:58:27 -0300

    Entrega Projeto-LabeX - Etapas 1-3 [Routes DOM]

--- modulo4/projeto-labex/labex/src/App.js
+++ modulo4/projeto-labex/labex/src/App.js
@@ -5,10 +5,23 @@ import ListTripsPage from './pages/ListTripsPage/ListTripsPage';
 import AdminHomePage from './pages/AdminHomePage/AdminHomePage';
 import ApplicationFormPage from './pages/ApplicationFormPage/ApplicationFormPage';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-      <AdminHomePage />
+      <BrowserRouter>
+      
+        <Routes>
+
+          <Route index element={<HomePage />} />
+          <Route path='lsit' element={<ListTripsPage />} />
+          <Route path='admhome' element={<AdminHomePage />} />
+          <Route path='application' element={<ApplicationFormPage />} />
+
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
