184221c478d4626b94afafcbaeceddf8a6700268
alainn-n <contato.alain@gmail.com>
2022-04-05 22:39:43 -0300

    Entrega Projeto-LabeX - Etapas 1-4 [Autenticação em aplicações React ]

--- modulo4/projeto-labex/labex/src/routes/Router.js
+++ modulo4/projeto-labex/labex/src/routes/Router.js
@@ -4,6 +4,7 @@ import HomePage from '../pages/HomePage/HomePage';
 import ListTripsPage from '../pages/ListTripsPage/ListTripsPage';
 import AdminHomePage from '../pages/AdminHomePage/AdminHomePage';
 import ApplicationFormPage from '../pages/ApplicationFormPage/ApplicationFormPage';
+import LoginPage from "../pages/LoginPage/LoginPage";
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -19,6 +20,7 @@ const Router = () => {
         <Route path='list' element={<ListTripsPage />} />
         <Route path='admhome' element={<AdminHomePage />} />
         <Route path='application' element={<ApplicationFormPage />} />
+        <Route path='login' element={<LoginPage />} />
 
       </Routes>
 
