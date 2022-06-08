be0387832e7abcfa3bfc40f5f6ca1c93ac442a91
alainn-n <contato.alain@gmail.com>
2022-06-07 23:26:01 -0300

    Entrega Exercicios Aula Fluxo de dados no Backend

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,28 @@
+type list = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const toDoList: list [] = [
+    {
+        id: 'sacolão',
+        name: 'alface',
+        price: 4.50
+    },
+    {
+        id: 'mercado',
+        name: 'morango',
+        price: 9.00
+    },
+    {
+        id: 'padaria',
+        name: 'chipa',
+        price: 5.00
+    },
+    {
+        id: 'açougue',
+        name: 'patinho moído',
+        price: 39.00
+    }
+]
